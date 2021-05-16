d5b322ccc83ea6c21179402766a95f2091ae3f6f
redkaio <chrno.92@gmail.com>
2021-05-16 18:41:52 -0300

    correcao da branch master

---
+++ src/pages/Header/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ChatHeader = styled.header`
+  box-sizing: border-box;
+  max-width: 900px;
+  font-size: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  margin: 0px;
+  background-color: #298A53;
+  color: #fff;
+  border-radius: 5px;
+`;
+
