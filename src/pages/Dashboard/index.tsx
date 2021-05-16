8e6ba21878c9b762197e35c0427b0470215ceb84
redkaio <chrno.92@gmail.com>
2021-05-16 13:35:01 -0300

    implementacao da persistencia em pagina dos repositorios salvos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import gitlogo from '../../assets/gitlogo.svg';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -15,27 +15,52 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubExplorer: repositories');
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@GithubExplorer: repositories',
+    JSON.stringify(repositories));
+  }, [repositories]);
+
+
+
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+
     event.preventDefault();
-    console.log(newRepo);
 
-    const response = await api.get<Repository>(`repos/${newRepo}`);
-    console.log(response.data);
+    if (!newRepo) {
+      setInputError('Digite o nome autor/nome do repositório');
+      return;
+    }
 
-    const repository = response.data;
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+      const repository = response.data;
 
-    setRepositories([...repositories, repository]);
-    setNewRepo('');
-  }
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório');
+    }
+};
 
   return (
     <>
       <img src={gitlogo} alt="Github Explorer" />
       <Title>Explore Repositórios no Github</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputError}onSubmit={handleAddRepository}>
         <input
           value={newRepo}
           onChange={e => setNewRepo(e.target.value)}
@@ -44,21 +69,23 @@ const Dashboard: React.FC = () => {
         <button type="submit">Pesquisar</button>
       </Form>
 
-      <Repositories>
-       {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
-          <img
-            src={repository.owner.avatar_url}
-            alt={repository.owner.login}
-          />
-          <div>
-            <strong>{repository.full_name}</strong>
-            <p>{repository.description}</p>
-          </div>
+      {inputError && <Error>{inputError}</Error>}
 
-          <FiChevronRight size={20} />
-        </a>
-       ))}
+      <Repositories>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
 
     </>
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from "styled-components";
-import { shade } from 'polished';
+import styled, { css } from "styled-components";
+import { shade } from "polished";
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -9,39 +13,50 @@ export const Title = styled.h1`
   line-height: 56px;
 `;
 
-export const Form = styled.form`
-margin-top: 40px;
-max-width: 700px;
-display: flex;
+export const Form = styled.form<FormProps>`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
 
-input {
-  flex: 1;
-  height: 70px;
-  padding: 0 24px;
-  border: 0;
-  border-radius: 5px 0 0 5px;
-  color: #3a3a3a;
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    border: 2px solid #fff;
 
-  &::placeholder {
-    color: #a8a8b3;
+    ${(props) => props.hasError && css`
+    border-color: #c53030;
+    border-right: 0;
+    `}
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
   }
 
-}
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    border-radius: 0px 5px 5px 0px;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
 
-button {
-  width: 210px;
-  height: 70px;
-  background: #04d361;
-  border-radius: 0px 5px 5px 0px;
-  border: 0;
-  color: #FFF;
-  font-weight: bold;
-  transition: background-color 0.2s;
-
-  &:hover {
-    background: ${shade(0.2, '#04d361')}
+    &:hover {
+      background: ${shade(0.2, "#04d361")};
+    }
   }
-}
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
@@ -51,7 +66,6 @@ export const Repositories = styled.div`
   a {
     background: white;
     border-radius: 5px;
-    width: 100%;
     padding: 24px;
     display: block;
     text-decoration: none;
@@ -68,8 +82,6 @@ export const Repositories = styled.div`
       margin-top: 16px;
     }
 
-
-
     img {
       width: 64px;
       height: 64px;
@@ -82,12 +94,12 @@ export const Repositories = styled.div`
 
       strong {
         font-size: 20px;
-        color: #3D3D4D;
+        color: #3d3d4d;
       }
 
       p {
         font-size: 18px;
-        color: #A8A8B3;
+        color: #a8a8b3;
         margin-top: 4px;
       }
     }
@@ -95,6 +107,5 @@ export const Repositories = styled.div`
       margin-left: auto;
       color: #cbcbd6;
     }
-
   }
 `;
