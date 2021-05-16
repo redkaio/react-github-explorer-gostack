93b11bc6e073e829c1ac5ef19886c7148c6276f6
redkaio <chrno.92@gmail.com>
2021-05-16 01:24:49 -0300

    adicao do consumo da api e revisao da estilizacao

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,58 +1,64 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import gitlogo from '../../assets/gitlogo.svg';
 import { Title, Form, Repositories } from './styles';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    console.log(newRepo);
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    console.log(response.data);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={gitlogo} alt="Github Explorer" />
       <Title>Explore Repositórios no Github</Title>
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
+       {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
           <img
-            src="https://avatars.githubusercontent.com/u/38678725?v=4"
-            alt="redkaio"
+            src={repository.owner.avatar_url}
+            alt={repository.owner.login}
           />
           <div>
-            <strong>redkaio/github</strong>
-            <p>My diploma says I am a Civil Engineer. My visit card says I am a developer. But my heart says I am a creator.</p>
+            <strong>{repository.full_name}</strong>
+            <p>{repository.description}</p>
           </div>
 
           <FiChevronRight size={20} />
         </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/38678725?v=4"
-            alt="redkaio"
-          />
-          <div>
-            <strong>redkaio/github</strong>
-            <p>My diploma says I am a Civil Engineer. My visit card says I am a developer. But my heart says I am a creator.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/38678725?v=4"
-            alt="redkaio"
-          />
-          <div>
-            <strong>redkaio/github</strong>
-            <p>My diploma says I am a Civil Engineer. My visit card says I am a developer. But my heart says I am a creator.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
+       ))}
       </Repositories>
 
     </>
